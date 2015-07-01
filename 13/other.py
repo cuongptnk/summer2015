1919ccc563e8a38ead09c0110fb657f254138c64
cuongptnk <nguyenphucuong250690@gmail.com>
2015-07-01 14:39:28 -0300

    Correct Add
    It is correct now

--- 13/other.py
+++ 13/other.py
@@ -10,35 +10,41 @@ result = "";
 
 
 
-while (end_num1 !=0) or (end_num2 != 0) :
-	if end_num1 == 0:
-		while end_num2 != 0 :
-			result = num2[end_num2] + result
-			end_num2 -=1
-	elif end_num2 == 0 :
-		while end_num1 != 0 :
-			result = num1[end_num1] + result
-			end_num1 -=1
+while (end_num1  >= 0) or (end_num2 >= 0) :
+	if end_num1 < 0:
+		if	int(num2[end_num2])+carry < 10:
+			tmp = int(num2[end_num2]) + carry
+			result = str(tmp) + result
+			carry =0
+		else :
+			tmp = int(num2[end_num2 ]) + carry - 10
+			result = str(tmp) + result
+			carry = 1
+	elif end_num2 < 0 :
+		if	int(num1[end_num1]) + carry < 10:
+			tmp = int(num1[end_num1 ]) + carry
+			result = str(tmp) + result
+			carry =0
+
+		else :
+			tmp = int(num1[end_num1 ])  + carry - 10
+			result = str(tmp) + result
+			carry = 1
 	else :
 		if	int(num1[end_num1]) + int(num2[end_num2])+carry < 10:
 			tmp = int(num1[end_num1 ]) + int(num2[end_num2]) + carry
 			result = str(tmp) + result
 			carry =0
-			tmp =0
-			
-			
-			end_num1 -=1
-			end_num2 -=1
+
 		else :
 			tmp = int(num1[end_num1 ]) + int(num2[end_num2 ]) + carry - 10
 			result = str(tmp) + result
 			carry = 1
-			tmp =0
+
 			
 		
-			end_num1 -=1
-			end_num2 -=1
+	end_num1 -=1
+	end_num2 -=1
 
 print result	
-print end_num1
-print end_num2	
+	
