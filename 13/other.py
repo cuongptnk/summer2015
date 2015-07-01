1cf1d622c35e3ec808835907b72063a4b13bda42
cuongptnk <nguyenphucuong250690@gmail.com>
2015-07-01 14:26:51 -0300

    Change condition while
    Can add two number with diff length

--- 13/other.py
+++ 13/other.py
@@ -1,8 +1,8 @@
 num1 = "15"
 num2 = "8"
 
-end_num1 = len(num1) 
-end_num2 = len(num2) 
+end_num1 = len(num1) - 1
+end_num2 = len(num2) - 1
 
 tmp =0
 carry=0
@@ -10,26 +10,34 @@ result = "";
 
 
 
-while (end_num1 !=0) and (end_num2 != 0) :
-
-	if	int(num1[end_num1 -1]) + int(num2[end_num2 -1])+carry < 10:
-		tmp = int(num1[end_num1 -1]) + int(num2[end_num2-1]) + carry
-		result = str(tmp) + result
-		carry =0
-		tmp =0
-		
-		
-		end_num1 -=1
-		end_num2 -=1
+while (end_num1 !=0) or (end_num2 != 0) :
+	if end_num1 == 0:
+		while end_num2 != 0 :
+			result = num2[end_num2] + result
+			end_num2 -=1
+	elif end_num2 == 0 :
+		while end_num1 != 0 :
+			result = num1[end_num1] + result
+			end_num1 -=1
 	else :
-		tmp = int(num1[end_num1 -1]) + int(num2[end_num2 -1]) + carry - 10
-		result = str(tmp) + result
-		carry = 1
-		tmp =0
+		if	int(num1[end_num1]) + int(num2[end_num2])+carry < 10:
+			tmp = int(num1[end_num1 ]) + int(num2[end_num2]) + carry
+			result = str(tmp) + result
+			carry =0
+			tmp =0
+			
+			
+			end_num1 -=1
+			end_num2 -=1
+		else :
+			tmp = int(num1[end_num1 ]) + int(num2[end_num2 ]) + carry - 10
+			result = str(tmp) + result
+			carry = 1
+			tmp =0
+			
 		
-	
-		end_num1 -=1
-		end_num2 -=1
+			end_num1 -=1
+			end_num2 -=1
 
 print result	
 print end_num1
